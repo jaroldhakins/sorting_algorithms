7eba13a0ea3ad2de57dbfa9ba67c6bb249168dd5
jaroldhakins <stivenhakins@gmail.com>
2021-10-28 23:18:31 +0000

    quick_sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,71 @@
+#include "sort.h"
+/**
+ * recursion - recursively does quick sort
+ * @array: the array to be sorted
+ * @start: the first element of the array
+ * @end: the pivot for the array
+ * @size: number of elements of the array
+ */
+void recursion(int *array, int start, int end, size_t size)
+{
+	size_t index = 0;
+
+	if (start < end)
+	{
+		index = midle(array, start, end, size);
+		if (index > 0)
+			recursion(array, start, index - 1, size);
+		if (index != size)
+			recursion(array, index + 1, end, size);
+	}
+}
+
+/**
+ * midle - it does Lomuto partition
+ * @array: the array to be sorted
+ * @start: the first element of the array
+ * @end: the pivot for the array
+ * @size: number of elements of the array
+ *
+ * Return: index for the sort
+ */
+int midle(int *array, int start, int end, size_t size)
+{
+	int p = array[end], index = start - 1, i, aux;
+
+	for (i = start; i < end; i++)
+	{
+		if (array[i] <= p)
+		{
+			index++;
+			if (index != i)
+			{
+				aux = array[i];
+				array[i] = array[index];
+				array[index] = aux;
+				print_array(array, size);
+			}
+		}
+	}
+	if (p < array[index + 1])
+	{
+		aux = array[index + 1];
+		array[index + 1] = array[end];
+		array[end] = aux;
+		print_array(array, size);
+	}
+	return (index + 1);
+}
+
+/**
+ * quick_sort - a function that sorts an array by quick sort
+ * @array: the array to be sorted
+ * @size: number of elements that the array has
+ */
+void quick_sort(int *array, size_t size)
+{
+	int start = 0, end = size - 1;
+
+	if (array != NULL && size != 0)
+		recursion(array, start, end, size);
+}
